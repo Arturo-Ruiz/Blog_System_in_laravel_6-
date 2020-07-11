1d13393378a3ae192be4646a733d8f14a6bf867d
Gregory <gregory09ruiz@gmail.com>
2020-07-11 16:14:22 -0400

    posts_ready

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(TagsTableSeeder::class);
-        $this->call(PotsTableSeeder::class);
+        $this->call(PostsTableSeeder ::class);
 
     }
 }
