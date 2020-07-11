5bb2b3bf985c60eda78d4257e6aeace484c0cfa5
Gregory <gregory09ruiz@gmail.com>
2020-07-11 16:16:38 -0400

    post_table_seeder_relacionship

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,6 +11,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Post::class, 300)->create();
+        factory(App\Post::class, 300)->create()->each(function(App\Post $post){
+            $post->tags()->attach([
+                random(1,5),
+                random(6,14),
+                random(15,20),
+            ]);
+        });
     }
 }
