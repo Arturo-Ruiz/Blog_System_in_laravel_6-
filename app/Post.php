a46c3d4f7d24694420885f648033fe7dc5139f50
Gregory <gregory09ruiz@gmail.com>
2020-07-11 16:42:33 -0400

    app\model__ready

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,18 @@ class Post extends Model
 {
 
 
+    protected $fillable = [
+        'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status', 'file'
+    ];
+
+    //Mediante esta funcion le digo a laravel que el post pertenece a un usuario
+    public function user(){
+        return $this->belongsTo(User::tag);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::tag);
+    }
 
     //Mediante esta funcion le digo a Laravel que existe una relacion de post y etiquetas
     //La relacion es de tipo muchos a muchos
@@ -18,5 +30,8 @@ class Post extends Model
     }
 
 
+    //Relaciones de la DB Tienen que estar aqui como funciones
+    // Si son multiples etiquetas, debe de ir en plurar
+    // Si son poco , Deben de ir en singular...
 
 }
