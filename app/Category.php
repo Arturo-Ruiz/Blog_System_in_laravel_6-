0966ade298c84e2ca87e47306e6fd2e5a89e6dd4
Gregory <gregory09ruiz@gmail.com>
2020-07-11 17:22:52 -0400

    category_model_ready

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+
+    protected $fillable = [
+        'name', 'slug', 'body'
+    ];
+
+
+    //Mediante esta funcion le digo a Laravel que existe una relacion de categorias y posts
+    //La relacion es de tipo muchos a muchos
+    //Con BelongsToMany Le digo que una categoria puede tener muchos posts
+    //la clase entre Parentesis
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
+
+
+    //Relaciones de la DB Tienen que estar aqui como funciones
+    // Si son multiples etiquetas, debe de ir en plurar
+    // Si son poco , Deben de ir en singular...
+
 }
