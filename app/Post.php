53250b1e31ea1e61b26d9d369fe40a7d42e48a15
Gregory <gregory09ruiz@gmail.com>
2020-07-11 16:20:48 -0400

    relacionship_model_posts_tags

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+
+
+    //Mediante esta funcion le digo a Laravel que existe una relacion de post y etiquetas
+    //La relacion es de tipo muchos a muchos
+    //Con BelongsToMany Le digo Que este post pertenece y tiene muchas etiquetas pasandole
+    //la clase entre Parentesis
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+
+
 }
