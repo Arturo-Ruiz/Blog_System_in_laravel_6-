2b03d74758811fb8de88b6d7c08f203248beafdd
Gregory <gregory09ruiz@gmail.com>
2020-07-11 15:21:06 -0400

    migration_tags_table

--- database/migrations/2020_07_11_185228_create_tags_table.php
+++ database/migrations/2020_07_11_185228_create_tags_table.php
@@ -15,6 +15,13 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->string('name', 128);
+            //Los Campos Unicos deben tener una longitud para seguir el estandar
+            //Al crear un campo unico lo que quiero decir es que su contenido no se puede repetir en esta tabla
+            $table->string('slug', 128)->unique;
+           
+
             $table->timestamps();
         });
     }
